2df1b4059d1a2f2f2c071a55dabedee7afd6683f
Saba Minashvili <sabaminashvili04@gmail.com>
2023-04-28 15:43:11 +0400

    Add validator for AuthenticationRequest

---
+++ BlogApp.API/Middlewares/Validation/Validations/AuthenticationRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using BlogApp.Application.Authentication.Models.Requests;
+using BlogApp.Shared.Localizations.Culture.Resources;
+using FluentValidation;
+
+namespace BlogApp.API.Middlewares.Validation.Validations;
+
+/// <summary>
+/// Represents a validator for the <see cref="AuthenticationRequestValidator"/> class.
+/// </summary>
+public class AuthenticationRequestValidator : AbstractValidator<AuthenticationRequest>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthenticationRequestValidator"/> class and
+    /// sets up validation rules for the Email and Password properties.
+    /// </summary>
+    public AuthenticationRequestValidator()
+    {
+        RuleFor(o => o.Email)
+            .NotNull()
+            .WithMessage(ValidationErrorMessages.NotNull)
+            .NotEmpty()
+            .WithMessage(ValidationErrorMessages.NotEmpty);
+
+        RuleFor(o => o.Password)
+            .NotNull()
+            .WithMessage(ValidationErrorMessages.NotNull)
+            .NotEmpty()
+            .WithMessage(ValidationErrorMessages.NotEmpty);
+    }
+}
